45d3a96ef698df1e10a1c29d230f3e27d2a76791
Grizzzotto <128230429+Grizzzotto@users.noreply.github.com>
2025-03-22 23:02:49 +0100

    Update binary_heap.py
    mission accomplished

--- binary_heap.py
+++ binary_heap.py
@@ -1,41 +1,64 @@
 from dataclasses import dataclass
-from typing import Any
 
 @dataclass
 class Element:
-    value: Any
-    priority: int
- 
+    value: object
+    prio: int
 
-class BinaryHeap:
+class PriorityQueue:
+    def __init__(self) -> None:
+        self.queue = []
 
-    def __init__(self):
-        self.heap = []
-    
     def push(self, element):
-        # naivni implementace binarni haldy
-        self.heap.append(element)
+        self.queue.append(element)
+        self.heap_up(len(self.queue) - 1)
+
+    def heap_up(self, index):
+        while index > 0:
+            parent_index = (index - 1) // 2
+            if self.queue[index] < self.queue[parent_index]:
+                self.queue[index], self.queue[parent_index] = self.queue[parent_index], self.queue[index]
+                index = parent_index
+            else:
+                break
 
     def pop(self):
-        # naivni implementace binarni haldy
-        # najdi nejmensi prvek a vrat ho
-        if not self.heap:
-            raise Exception("Heap is empty")
-        min_idx = 0
-        for idx in range(1,len(self.heap)):
-            if self.heap[idx].priority < self.heap[min_idx].priority:
-                min_idx = idx
-        return self.heap.pop(min_idx)
+        if not self.queue:
+            return None
+        if len(self.queue) == 1:
+            return self.queue.pop()
+
+        root = self.queue[0]
+        self.queue[0] = self.queue.pop()
+        self.heap_down(0)
+        return root
+
+    def heap_down(self, index):
+        size = len(self.queue)
+        while True:
+            left_child = 2 * index + 1
+            right_child = 2 * index + 2
+            smallest = index
+            if left_child < size and self.queue[left_child] < self.queue[smallest]:
+                smallest = left_child
+            if right_child < size and self.queue[right_child] < self.queue[smallest]:
+                smallest = right_child
+            if smallest != index:
+                self.queue[index], self.queue[smallest] = self.queue[smallest], self.queue[index]
+                index = smallest
+            else:
+                break
 
 
-    def head(self):
-        if not self.heap:
-            raise Exception("Heap is empty")
-        # vrati nejmensi element ve fronte (element na cele fronty)
-        # protoze mame naivni implementaci, musime projit cely seznam
-        min_idx = 0
-        for idx in range(1,len(self.heap)):
-            if self.heap[idx].priority < self.heap[min_idx].priority:
-                min_idx = idx
-        return self.heap[min_idx]
-        
+pr = PriorityQueue()
+pr.push(3)
+pr.push(5)
+pr.push(2)
+pr.push(4)
+pr.push(6)
+pr.push(7)
+pr.push(9)
+pr.push(1)
+print("heap:", pr.queue)
+print("pop:", pr.pop())
+print("heap:", pr.queue)
